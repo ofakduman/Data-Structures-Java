bcfd6ba44c9a0d27f8414a44ad3215aad3f78cb0
Ömer F. Akduman <61577706+ofakduman@users.noreply.github.com>
2022-02-17 18:30:35 +0300

    update2

--- ch6/6-4/src/com/company/Programming.java
+++ ch6/6-4/src/com/company/Programming.java
@@ -7,18 +7,17 @@ import java.util.function.BiFunction;
  * 1. Write a lambda expression that takes a double argument (x) and an integer argument (n).
  * The method result should be the double value raised to the power n. Do this using a Java
  * API method and also using a loop.
- * 2. Write and test a Java class that enters two numbers and displays the result of calling each
- * lambda expression in Programming Exercise 1. Also, compare the results to make sure
- * that they are the same.
- * 3. Modify the program in Example 6.6 to use two function objects that calculate trigonometric values
- * and display the angle and corresponding values for each function object on the
- * same line.
- * 4. Write a general postOrderTraverse method for the BinaryTree class similar to the
+ * 4. Write a general postOrderTraverse method for the BinaryTree class similar to the (Check BinaryTree.java file)
  * preOrderTraverse method.
- * 5. Write a general inOrderTraverse method for the BinaryTree class similar to the preOrderTraverse method.
+ * 5. Write a general inOrderTraverse method for the BinaryTree class similar to the preOrderTraverse method. (Check BinaryTree.java file)
  */
 public class Programming {
     public static void main(String[] args){
+        /**
+         *  * PROGRAMMING
+         *  * 1. Write a lambda expression that takes a double argument (x) and an integer argument (n).
+         *  * The method result should be the double value raised to the power n. Do this using a Java
+         */
         BiFunction<Double, Integer, Double> lambda = (x,n)->{
             Double result = 1.0;
             for (int i = 0; i < n; i++) {
