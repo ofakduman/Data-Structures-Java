d81bdcc4b88f875cb753e04d4aeafe4d65999e33
Ömer F. Akduman <61577706+ofakduman@users.noreply.github.com>
2022-02-18 12:20:57 +0300

    BinarySearchTree added

---
+++ ch6/6-5/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.TreeSet;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BinaryTree<Integer> iii = new BinaryTree<>();
+        TreeSet<Integer> ts = new TreeSet<>();
+    }
+}
---
+++ ch6/6-5/src/com/company/SearchTree.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public interface SearchTree <E>{
+    boolean add(E item);
+    boolean contains(E target);
+    E find(E target);
+    E delete(E target);
+    boolean remove(E target);
+}
